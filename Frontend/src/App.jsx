c233a67ebfa98a076bd1852e5fcd1feaf76fe83b
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2023-01-05 18:40:46 -0600

    added topbooks page still needa get the api running and the css organized

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import TopBooks from './pages/TopBooks';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />} />
+        <Route path = "/topbooks" element={<TopBooks />} />
       </Routes>
     </div>
     </Router>
