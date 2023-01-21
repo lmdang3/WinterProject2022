515efc269a2a42295be95cb3e774450ea294c9c8
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2023-01-21 14:58:22 -0600

    login state management changed
    After consulting matt, I have decided to go back to using react query for the management of the user data thats logged in. With the inclusion of a token to handle the status of the user which will be stored in the session storage.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Navbar from './components/Navbar';
@@ -12,8 +12,12 @@ import Docs from './pages/Docs';
 import { RegisterForm } from './components/Register';
 import Thank from './pages/ThankYouPage';
 // import Test from './components/test';
+
+// Setting up React Query to handle some of some reusablity 
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { EmailContext, PasswordContext } from './context'
+
+// saving some of my Context data but could use if need be 
+// import { EmailContext, PasswordContext } from './context'
 
 
 const client = new QueryClient()
@@ -23,37 +27,37 @@ function App() {
   const [password, setPassword] = useState('')
   return (
     <QueryClientProvider client={client}>
-      <EmailContext.Provider value={email}>
-        <PasswordContext.Provider value={password}>
-          <Router>
-            <div className="pt-20">
-              <Navbar />
+      {/* <EmailContext.Provider value={email}>
+        <PasswordContext.Provider value={password}> */}
+      <Router>
+        <div className="pt-20">
+          <Navbar />
 
-              <Routes>
-                {/* sets up the routes */}
+          <Routes>
+            {/* sets up the routes */}
 
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<AboutMe />} />
-                <Route path="/topbooks" element={<GetRequestHooks />} />
-                <Route path="/bookreviews" element={<BookReviews />} />
-                <Route path="/nav" element={<Navbar />} />
-                <Route path="/docs" element={<Docs />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/topbooks" element={<GetRequestHooks />} />
+            <Route path="/bookreviews" element={<BookReviews />} />
+            <Route path="/nav" element={<Navbar />} />
+            <Route path="/docs" element={<Docs />} />
 
-                {/* Take to the login */}
-                <Route path="/login" element={<LoginForm />} />
-                {/* Takes to create a new acct */}
-                <Route path="/register" element={<RegisterForm />} />
-                <Route path="/thank" element={<Thank />} />
-                {/* <Route path="test" element={<Test />} />
+            {/* Take to the login */}
+            <Route path="/login" element={<LoginForm />} />
+            {/* Takes to create a new acct */}
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/thank" element={<Thank />} />
+            {/* <Route path="test" element={<Test />} />
          */}
 
-              </Routes>
-            </div>
-          </Router>
-</PasswordContext.Provider>
-</EmailContext.Provider>
-</QueryClientProvider>
-        )
+          </Routes>
+        </div>
+      </Router>
+      {/* </PasswordContext.Provider>
+</EmailContext.Provider> */}
+    </QueryClientProvider>
+  )
 }
 
-        export default App
+export default App
